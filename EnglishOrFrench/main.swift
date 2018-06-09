c96553620ea195a2ff25bfec33f813246e84e8ca
Julius Grosse <jgrosse@lcs.on.ca>
2018-06-09 15:29:15 -0400

    Almost done, still struggling with the line input <1 or >100

--- EnglishOrFrench/main.swift
+++ EnglishOrFrench/main.swift
@@ -27,25 +27,49 @@ while (true) {
 // Implement the primary logic of the problem here
 // Some output may be given here if you desire
 // Now get the actual lines
+var englishCounter = 0
+var frenchCounter = 0
+var lineInput = ""
+
 print("Enter \(n) lines of text:")
 
 for counter in 1...n {
     
     print("Line \(counter)")
     
-    guard let lineInput = readLine() else {
+    guard let input = readLine() else {
         
         continue
         
     }
     
-    print(lineInput)
-    let englishCounter = lineInput
-    print(englishCounter)
+    lineInput = input
+    
+    if lineInput.count < 1 || lineInput.count > 100 {
+        
+        continue
+        
+    }
+    
+    for character in lineInput {
+        
+        if character == "T" || character == "t" {
+            
+            englishCounter += 1
+            
+        } else if character == "S" || character == "s" {
+            
+            frenchCounter += 1
+            
+        }
+    }
 }
-
 
 // OUTPUT
 // Report results to the user here
-
+if englishCounter > frenchCounter {
+    print("This is probably English.")
+} else if frenchCounter == englishCounter || frenchCounter > englishCounter {
+    print("This is probably French.")
+}
 
